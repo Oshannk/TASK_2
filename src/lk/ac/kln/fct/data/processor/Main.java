fa81799db78349023e1636a1a9e6c9a17a32de02
Oshannk <oshanmuththettpita@oulook.com>
2020-05-29 16:50:08 +0530

    modify

--- src/lk/ac/kln/fct/data/processor/Main.java
+++ src/lk/ac/kln/fct/data/processor/Main.java
@@ -5,22 +5,21 @@ import lk.ac.kln.fct.data.pojo.Record;
 import java.util.ArrayList;
 
 public class Main {
-	private static final String FILE_PATH = "src/covid19data.json";
-	private static final String DATE = "18/05/2020";
-	
-	public static void main(String[] args) {
-		DataProcessor processor = new DataProcessor();
-		sortAndPrint(processor.getDataByDate(FILE_PATH,DATE));
-	}
-		
-	public static void sortAndPrint(ArrayList<Record> records) {
-		records.sort((record1, record2)-> Integer.compare(record2.getCases(), record1.getCases()));
-		for(int i =0; i < records.size(); i ++){
-			//System.out.println(records.get(i).getCases());
-			if(records.get(i).getCases()>=1000){
-				System.out.println(records.get(i).getCountriesAndTerritories()+" : "+records.get(i).getCases()+" : "+records.get(i).getDeaths());
-			}
-		}
+    private static final String FILE_PATH = "src/covid19data.json";
+    private static final String DATE = "18/05/2020";
 
-	}
+    public static void main(String[] args) {
+        DataProcessor processor = new DataProcessor();
+        sortAndPrint(processor.getDataByDate(FILE_PATH, DATE));
+    }
+
+    public static void sortAndPrint(ArrayList<Record> records) {
+        records.sort((record1, record2) -> Integer.compare(record2.getCases(), record1.getCases()));
+        for (int i = 0; i < records.size(); i++) {
+            if (records.get(i).getCases() >= 1000) {
+                System.out.println(records.get(i).getCountriesAndTerritories() + " : " + records.get(i).getCases() + " : " + records.get(i).getDeaths());
+            }
+        }
+
+    }
 }
